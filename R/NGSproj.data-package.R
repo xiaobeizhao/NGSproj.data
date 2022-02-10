71e1080ef582624d8440c264d2359c5f90e4b90c
xiaobeizhao <xiaobei@login01.uthsc.edu>
2022-02-10 13:46:17 -0600

    first commit

---
+++ R/NGSproj.data-package.R
@@ -0,0 +1,34 @@
+
+## ************************************************************************
+## NGSproj.data-package.R: Roxygen2 documentation file for the NGSproj.data package.
+## 
+## 
+## (c) Xiaobei Zhao
+## 
+## Thu Feb 10 13:12:34 CST 2022 -0600 (Week 06)
+## ## To generate
+## roxygen2::roxygenize(".")
+##
+## 
+## Reference: 
+## 
+## 
+## ************************************************************************
+
+
+##' Xiaobei's collected datasets for NGS projects
+##' 
+##' Listed are a few highlighted sets
+##' \itemize{
+##' \item \code{\link{...}}
+##' }
+##'
+##' You may find the latest version of \code{NGSproj.data} at \url{https://github.com/xiaobeizhao/NGSproj.data}.
+##' @title Xiaobei's collected datasets for NGS projects
+##' @name NGSproj.data-package
+##' @docType package
+##' @author Xiaobei Zhao
+##' @references Xiaobei Zhao (2022)
+##' @keywords package
+NULL
+
---
+++ R/data.R
@@ -0,0 +1,37 @@
+
+## ************************************************************************
+## 
+## 
+## 
+## (c) Xiaobei Zhao
+## 
+## Mon Feb 07 18:04:22 CST 2022 -0600 (Week 06)
+## 
+## 
+## Reference: 
+## 
+## 
+## ************************************************************************
+
+
+
+##' test dataset
+##'
+##' A dataset containing ...
+##' 
+##' @title test dataset
+##' @return data.table
+##' @author
+##' @docType data
+##' @keywords datasets
+##' @name test
+##' @usage data(test)
+##' @format A data table with x rows and y columns:
+##' \itemize{
+##'   \item{}
+##'   \item{}
+##'   \item{}
+##' }
+##' @source \url{...}
+##' 
+NULL
